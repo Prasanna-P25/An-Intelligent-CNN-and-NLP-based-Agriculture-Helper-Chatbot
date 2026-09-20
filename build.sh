54a7c6ea4aa1bc21bc96f96bcc36c174a2d0b1f3
Prasanna <prasannapannala@gmail.com>
2026-09-20 17:43:11 +0530

    Pin pip for Python 3.7

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade "pip==23.3.2"
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
